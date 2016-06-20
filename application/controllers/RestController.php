7b0fca5f59e9800f4595e923c04836be739382ba
Gintautas Miselis <gintautas@miselis.lt>
2016-06-20 21:28:56 +0100

    Implemented tests for simple file upload

--- application/controllers/RestController.php
+++ application/controllers/RestController.php
@@ -15,13 +15,61 @@ class RestController extends Zend_Controller_Action
             'rawBody' => $this->_request->getRawBody(),
             'headers' => $this->_request->getHeaders(),
             'X-Auth-Token' => $this->_request->getHeader('X-Auth-Token'),
-            'files' => $_FILES,
+            'files' => $this->mapPhpFiles(),
         );
         
         $this->_response
             ->setHeader('Content-Type','application/json')
             ->setBody(json_encode($data));
 	
+    }
+
+
+
+    /**
+     * Convert PHP superglobal $_FILES into more sane parameter=value structure
+     * This handles form file input with brackets (name=files[])
+     *
+     * Copied from ZF2: \Zend\Http\PhpEnvironment\Request
+     *
+     * @return array
+     */
+    private function mapPhpFiles()
+    {
+        $files = [];
+        foreach ($_FILES as $fileName => $fileParams) {
+            $files[$fileName] = [];
+            foreach ($fileParams as $param => $data) {
+                if (!is_array($data)) {
+                    $files[$fileName][$param] = $data;
+                } else {
+                    foreach ($data as $i => $v) {
+                        $this->mapPhpFileParam($files[$fileName], $param, $i, $v);
+                    }
+                }
+            }
+        }
+
+        return $files;
+    }
+
+    /**
+     * Copied from ZF2: \Zend\Http\PhpEnvironment\Request
+     *
+     * @param array        $array
+     * @param string       $paramName
+     * @param int|string   $index
+     * @param string|array $value
+     */
+    private  function mapPhpFileParam(&$array, $paramName, $index, $value)
+    {
+        if (!is_array($value)) {
+            $array[$index][$paramName] = $value;
+        } else {
+            foreach ($value as $i => $v) {
+                $this->mapPhpFileParam($array[$index], $paramName, $i, $v);
+            }
+        }
     }
 }
 
